caea883dd7e1f54ed4455d7a2aad6295879105fb
outamghartAchraf <outamghart2000@gmail.com>
2026-04-14 14:45:22 +0100

    create table roles

--- structure.sql
+++ structure.sql
@@ -1,3 +1,11 @@
 /* -----create data base  -----*/
 CREATE DATABASE IF NOT EXISTS edusync;
 USE edusync;
+
+/* -----create data base  -----*/
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    role VARCHAR(255) NOT NULL
+);
+
+
