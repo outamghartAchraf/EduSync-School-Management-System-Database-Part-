90bf7677f1e1923b116c9837e21f2c21145250b5
outamghartAchraf <outamghart2000@gmail.com>
2026-04-14 14:51:48 +0100

    create table users

--- structure.sql
+++ structure.sql
@@ -2,10 +2,26 @@
 CREATE DATABASE IF NOT EXISTS edusync;
 USE edusync;
 
-/* -----create data base  -----*/
-CREATE TABLE roles(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    role VARCHAR(255) NOT NULL
+/* -----create table roles -----*/
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(50) NOT NULL UNIQUE
 );
+
+/* -----create table users -----*/
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role_id INT NOT NULL,
+
+    FOREIGN KEY (role_id)
+        REFERENCES roles(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
 
 
