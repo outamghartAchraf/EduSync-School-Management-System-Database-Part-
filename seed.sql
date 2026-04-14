cbd43a1fb11b9ba43d7e58c19054c53115957360
outamghartAchraf <outamghart2000@gmail.com>
2026-04-14 15:29:34 +0100

    insert data in table students

--- seed.sql
+++ seed.sql
@@ -19,5 +19,9 @@ INSERT INTO classes (name, classroom_number) VALUES
 ('MERN Stack 2026', 'B1'),
 ('JAVA  2026', 'A1');
 
-
+/*---- STUDENTS ------ */
+INSERT INTO students (date_of_birth, student_number, user_id, class_id) VALUES
+('2001-05-10', 'S01', 4, 1),
+('2000-11-22', 'S02', 5, 1),
+('2002-03-15', 'S03', 6, 2);
 
