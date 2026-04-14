dd067cfb89a1f15d69676fea1386976bfc7811e6
outamghartAchraf <outamghart2000@gmail.com>
2026-04-14 15:24:33 +0100

    insert data in table classes

--- seed.sql
+++ seed.sql
@@ -5,12 +5,19 @@ INSERT INTO roles (role_name) VALUES ('Admin'), ('Professor'), ('Student');
 
 
 /*---- USERS ------ */
-
 INSERT INTO users (firstname, lastname, email, password, role_id) VALUES
 ('Admin', 'System', 'admin@gmail.com', 'admin123', 1),
-('Ahmed', 'hari', 'ahmed@gmail.com.com', 'ahmed123', 2),
+('Ahmed', 'hari', 'ahmed@gmail.com', 'ahmed123', 2),
 ('hicham', 'benAmrani', 'hicham@gmail.com', 'hicham123', 2),
 ('achraf', 'outamghart', 'achraf@gmail.com', 'achraf123', 3),
-('Salma', 'idris', 'salma.idris@gmail.com', 'salma123', 3),
- 
+('Salma', 'idris', 'salma@gmail.com', 'salma123', 3),
+('SALAH', 'idris', 'SALAH@gmail.com', 'SALAH123', 3);
+
+/*---- CLASSES ------ */
+INSERT INTO classes (name, classroom_number) VALUES
+('PHP LARAVEL 2026', 'A1'),
+('MERN Stack 2026', 'B1'),
+('JAVA  2026', 'A1');
+
+
 
