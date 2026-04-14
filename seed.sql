eb66850d81216087d3bf367210ac0f5e03ba4a2e
outamghartAchraf <outamghart2000@gmail.com>
2026-04-14 15:19:13 +0100

    insert data of table users

--- seed.sql
+++ seed.sql
@@ -1,5 +1,16 @@
 USE edusync;
 
-/*--- ROLES------- */
+/*--- ROLES ------- */
 INSERT INTO roles (role_name) VALUES ('Admin'), ('Professor'), ('Student');
+
+
+/*---- USERS ------ */
+
+INSERT INTO users (firstname, lastname, email, password, role_id) VALUES
+('Admin', 'System', 'admin@gmail.com', 'admin123', 1),
+('Ahmed', 'hari', 'ahmed@gmail.com.com', 'ahmed123', 2),
+('hicham', 'benAmrani', 'hicham@gmail.com', 'hicham123', 2),
+('achraf', 'outamghart', 'achraf@gmail.com', 'achraf123', 3),
+('Salma', 'idris', 'salma.idris@gmail.com', 'salma123', 3),
+ 
 
